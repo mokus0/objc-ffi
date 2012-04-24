54bfe0c9268904577d0adc514f28e3db9c793c07
James Cook <mokus@deepbondi.net>
2012-04-24 15:02:16 -0400

    Add function to re-wrap exceptions

--- src/Foreign/ObjC/Exception.hs
+++ src/Foreign/ObjC/Exception.hs
@@ -1,6 +1,7 @@
 module Foreign.ObjC.Exception where
 
 import Control.Exception
+import Foreign.C.String
 import Foreign.C.Types
 import Foreign.LibFFI.Experimental
 import Foreign.Marshal.Alloc
@@ -9,6 +10,7 @@ import Foreign.ObjC.Types
 import Foreign.Ptr
 import Foreign.StablePtr
 import Foreign.Storable
+import System.IO.Unsafe
 
 unwrapException :: (Ptr ObjCException -> IO CInt) -> IO ()
 unwrapException action = alloca $ \exc -> do
@@ -23,6 +25,13 @@ unwrapException action = alloca $ \exc -> do
             deRefStablePtr sptr >>= throwIO
         2 -> do
             peek exc >>= throwIO
+
+wrapException :: IO () -> IO ObjCException
+wrapException action = catches (action >> return (ObjCException nullPtr))
+    [ Handler $ \p@ObjCException{} -> return p
+    , Handler $ \e@SomeException{} -> Raw.newHSException hsExceptionString =<< newStablePtr e
+    ]
+    where hsExceptionString = unsafePerformIO (newCString "HSException")
 
 objc_ffi_call :: CIF a -> FunPtr a -> Ptr (SigReturn a) -> Ptr (Ptr ()) -> IO ()
 objc_ffi_call cif impl ret args =
--- src/Foreign/ObjC/Raw/Exception.hs
+++ src/Foreign/ObjC/Raw/Exception.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Foreign.ObjC.Raw.Exception where
 
+import Control.Exception
+import Foreign.C.String
 import Foreign.C.Types
 import Foreign.LibFFI.Experimental
 import Foreign.ObjC.Types
 import Foreign.Ptr
+import Foreign.StablePtr
 
 foreign import ccall
     ffi_call_with_exceptions :: CIF a -> FunPtr a -> Ptr (SigReturn a) -> Ptr (Ptr ()) -> Ptr ObjCException -> IO CInt
+
+foreign import ccall
+    newHSException :: CString -> StablePtr SomeException -> IO ObjCException
